09147cadfed9e237856594a13495f5e39a5d4f22
Witold Wiśniewski <witold.wisniewski@escolasoft.com>
2026-03-23 16:59:50 +0100

    fix service

--- src/Services/TermAnalyticService.php
+++ src/Services/TermAnalyticService.php
@@ -23,17 +23,14 @@ class TermAnalyticService implements TermAnalyticServiceContract
 
     public function rebuildTermAnalytic(string $modelType, int $modelId, Carbon $term): void
     {
-        $termAnalytic = TermAnalytic::query()->where('model_type', $modelType)
-            ->where('model_id', $modelId)
-            ->where('term', $term)
-            ->first();
-        $query = AggregatedFrame::query()
-            ->where('model_type', $modelType)
-            ->where('model_id', $modelId)
-            ->where('term', $term);
+        DB::transaction(function () use ($modelType, $modelId, $term) {
+            $query = AggregatedFrame::query()
+                ->where('model_type', $modelType)
+                ->where('model_id', $modelId)
+                ->where('term', $term);
 
-        $data = $query
-            ->selectRaw("
+            $data = $query
+                ->selectRaw("
                 SUM(count) as count,
                 COUNT(id) as aggregated_frames_count,
                 MAX(updated_at) as last_frame_at,
@@ -47,66 +44,62 @@ class TermAnalyticService implements TermAnalyticServiceContract
                 SUM(sum_emotions_sad) as sum_emotions_sad,
                 SUM(sum_emotions_surprised) as sum_emotions_surprised
             ")
-            ->first();
+                ->first();
 
-        if (!$data) {
-            return;
-        }
+            if (!$data || $data->count == 0) {
+                return;
+            }
 
-        if (!$termAnalytic) {
-            $termAnalytic = new TermAnalytic();
-
-            $termAnalytic->model_type = $modelType;
-            $termAnalytic->model_id = $modelId;
-            $termAnalytic->term = $term;
-        }
-
-        $termAnalytic->count = $data->count;
-        $termAnalytic->aggregated_frames_count = $data->aggregated_frames_count;
-        $termAnalytic->sum_attention = $data->sum_attention;
-        $termAnalytic->sum_emotions_angry = $data->sum_emotions_angry;
-        $termAnalytic->sum_emotions_disgusted = $data->sum_emotions_disgusted;
-        $termAnalytic->sum_emotions_fearful = $data->sum_emotions_fearful;
-        $termAnalytic->sum_emotions_happy = $data->sum_emotions_happy;
-        $termAnalytic->sum_emotions_neutral = $data->sum_emotions_neutral;
-        $termAnalytic->sum_emotions_sad = $data->sum_emotions_sad;
-        $termAnalytic->sum_emotions_surprised = $data->sum_emotions_surprised;
-        $termAnalytic->last_frame_at = $data->last_frame_at;
-        $termAnalytic->avg_attention = $termAnalytic->sum_attention / $termAnalytic->count;
-        $termAnalytic->avg_emotions_angry = $termAnalytic->sum_emotions_angry / $termAnalytic->count;
-        $termAnalytic->avg_emotions_disgusted = $termAnalytic->sum_emotions_disgusted / $termAnalytic->count;
-        $termAnalytic->avg_emotions_fearful = $termAnalytic->sum_emotions_fearful / $termAnalytic->count;
-        $termAnalytic->avg_emotions_happy = $termAnalytic->sum_emotions_happy / $termAnalytic->count;
-        $termAnalytic->avg_emotions_neutral = $termAnalytic->sum_emotions_neutral / $termAnalytic->count;
-        $termAnalytic->avg_emotions_sad = $termAnalytic->sum_emotions_sad / $termAnalytic->count;
-        $termAnalytic->avg_emotions_surprised = $termAnalytic->sum_emotions_surprised / $termAnalytic->count;
-
-        $emotions = collect([
-            EmotionsEnum::ANGRY => $termAnalytic->avg_emotions_angry,
-            EmotionsEnum::DISGUSTED => $termAnalytic->avg_emotions_disgusted,
-            EmotionsEnum::FEARFUL => $termAnalytic->avg_emotions_fearful,
-            EmotionsEnum::HAPPY => $termAnalytic->avg_emotions_happy,
-            EmotionsEnum::NEUTRAL => $termAnalytic->avg_emotions_neutral,
-            EmotionsEnum::SAD => $termAnalytic->avg_emotions_sad,
-            EmotionsEnum::SURPRISED => $termAnalytic->avg_emotions_surprised,
-        ]);
-
-        $maxEmotion = $emotions->sortDesc()->keys()->first();
-        $maxEmotionValue = $emotions->get($maxEmotion);
-
-        $termAnalytic->max_emotion = $maxEmotion;
-        $termAnalytic->max_emotion_value = $maxEmotionValue;
-
-        $termAnalytic->save();
-
-        AggregatedFrame::query()
-            ->where('model_type', $modelType)
-            ->where('model_id', $modelId)
-            ->where('term', $term)
-            ->whereNull('term_analytic_id')
-            ->update([
-                'term_analytic_id' => $termAnalytic->getKey(),
+            $emotions = collect([
+                EmotionsEnum::ANGRY => $data->sum_emotions_angry,
+                EmotionsEnum::DISGUSTED => $data->sum_emotions_disgusted,
+                EmotionsEnum::FEARFUL => $data->sum_emotions_fearful,
+                EmotionsEnum::HAPPY => $data->sum_emotions_happy,
+                EmotionsEnum::NEUTRAL => $data->sum_emotions_neutral,
+                EmotionsEnum::SAD => $data->sum_emotions_sad,
+                EmotionsEnum::SURPRISED => $data->sum_emotions_surprised,
             ]);
+
+            $maxEmotion = $emotions->sortDesc()->keys()->first();
+
+            $termAnalytic = TermAnalytic::updateOrCreate(
+                ['model_type' => $modelType, 'model_id' => $modelId, 'term' => $term],
+                [
+                    'count' => $data->count,
+                    'aggregated_frames_count' => $data->aggregated_frames_count,
+                    'sum_attention' => $data->sum_attention,
+
+                    'sum_emotions_angry' => $data->sum_emotions_angry,
+                    'sum_emotions_disgusted' => $data->sum_emotions_disgusted,
+                    'sum_emotions_fearful' => $data->sum_emotions_fearful,
+                    'sum_emotions_happy' => $data->sum_emotions_happy,
+                    'sum_emotions_neutral' => $data->sum_emotions_neutral,
+                    'sum_emotions_sad' => $data->sum_emotions_sad,
+                    'sum_emotions_surprised' => $data->sum_emotions_surprised,
+                    'avg_attention' => $data->sum_attention / $data->count,
+                    'avg_emotions_angry' => $data->sum_emotions_angry / $data->count,
+                    'avg_emotions_disgusted' => $data->sum_emotions_disgusted / $data->count,
+                    'avg_emotions_fearful' => $data->sum_emotions_fearful / $data->count,
+                    'avg_emotions_happy' => $data->sum_emotions_happy / $data->count,
+                    'avg_emotions_neutral' => $data->sum_emotions_neutral / $data->count,
+                    'avg_emotions_sad' => $data->sum_emotions_sad / $data->count,
+                    'avg_emotions_surprised' => $data->sum_emotions_surprised / $data->count,
+
+                    'max_emotion' => $maxEmotion,
+                    'max_emotion_value' => $emotions->get($maxEmotion) / $data->count,
+                    'last_frame_at' => $data->last_frame_at,
+                ]
+            );
+
+            AggregatedFrame::query()
+                ->where('model_type', $modelType)
+                ->where('model_id', $modelId)
+                ->where('term', $term)
+                ->whereNull('term_analytic_id')
+                ->update([
+                    'term_analytic_id' => $termAnalytic->getKey(),
+                ]);
+        });
     }
 
     public function termAnalyticsList(string $modelType, TermAnalyticsFilterListDto $criteriaDto, PageDto $pageDto, OrderDto $orderDto): LengthAwarePaginator
