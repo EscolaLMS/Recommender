07708608257b876fb8b83202c34444765fd74462
Witold Wiśniewski <witold.wisniewski@escolasoft.com>
2026-04-08 12:13:29 +0200

    Added url_expires_at to resource

--- src/Http/Resources/TermAnalyticResource.php
+++ src/Http/Resources/TermAnalyticResource.php
@@ -106,6 +106,7 @@ class TermAnalyticResource extends JsonResource
             'max_emotion_value' => $this->resource->max_emotion_value,
             'rating' => $this->resource->rating ?? random_int(1, 5),
             'url' => $this->resource->meetRecording && $this->resource->meetRecording->is_url_valid ? $this->resource->meetRecording->url : null,
+            'url_expires_at' => $this->resource->is_url_valid ? $this->resource->meetRecording->url_expires_at : null,
             'start_at' => $this->resource->meetRecording->start_at ?? null,
             'end_at' => $this->resource->meetRecording->end_at ?? null,
         ];
